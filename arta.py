7594917cf8f72dec4d5c1183588046e2138e12af
Michael Figueroa <michael.figueroa73@gmail.com>
2020-11-11 15:41:12 -0800

    push for pi

--- arta.py
+++ arta.py
@@ -7,10 +7,14 @@ from azure.cognitiveservices.vision.computervision.models import OperationStatus
 from azure.cognitiveservices.vision.computervision.models import VisualFeatureTypes
 from msrest.authentication import CognitiveServicesCredentials
 
+import cv2
 from dotenv import load_dotenv
-from pynput.keyboard import Listener
+import pafy
+from pynput import keyboard
 from modules import azure_manager
 from modules import camera_vlc
+
+load_dotenv()
 
 
 _time_start = datetime.datetime.now()
@@ -44,18 +48,55 @@ if not endpoint:
     print("\nSet the COMPUTER_VISION_ENDPOINT environment variable.\n**Restart your shell or IDE for changes to take effect.**")
     sys.exit()
 
-
 computervision_client = ComputerVisionClient(
     endpoint,
     CognitiveServicesCredentials(subscription_key))
 
-load_dotenv()
+url = 'https://www.youtube.com/watch?v=KmWr_p20tCM'
+vPafy = pafy.new(url)
+play = vPafy.getbest()
 
+_vid = cv2.VideoCapture(play.url)
 
+while True:
+    success, frame = _vid.read()
 
-'''
-Tag an Image - remote
-This example returns a tag (key word) for each thing in the image.
-'''
+    if success:
+        container = 'arta-rtsp-feed'
+        file_name = f'{_calc_time_diff(_time_start).seconds}.jpg'
+        path = f'./data/frames/'
+        camera = camera_vlc.CameraVLC(f'{path}{file_name}')
 
-print("===== Tag an image - remote =====")
+        camera.capture(frame)
+        az_manager = azure_manager.AzureManager()
+        cv2.destroyAllWindows()
+        az_manager.upload_image(
+            file_name,
+            path,
+            container)
+        
+        '''
+        Tag an Image - remote
+        This example returns a tag (key word) for each thing in the image.
+        '''
+        threat_words = ['gun', 'weapon', 'rifle', 'firearm']
+        threats = []
+        list_guns = az_manager.list_blobs_names(container)
+        # Call API with remote image
+        for item in list_guns:
+            print(f'===== Tag an image - remote ===== {item}')
+            tags_result_remote = computervision_client.tag_image(
+                az_manager.get_blob_url(container, item))
+
+            # Print results with confidence score
+            print("Tags in the remote image: ")
+            if (len(tags_result_remote.tags) == 0):
+                print("No tags detected.")
+            else:
+                for tag in tags_result_remote.tags:
+                    print("'{}' with confidence {:.2f}%".format(tag.name, tag.confidence * 100))
+
+                    if tag.name in threat_words:
+                        threats.append(f'{item}')
+                        break
+
--- modules/camera_vlc.py
+++ modules/camera_vlc.py
@@ -6,32 +6,20 @@ from pynput.keyboard import Listener
 
 class CameraVLC():
 
-    def __init__(self, rtsp_path, out_path):
-        self.rtsp_path = rtsp_path
+    def __init__(self, out_path):
         self.out_path = out_path
 
-    def on_press(self, key):
-        if hasattr(key, 'char'):
-            if key.char == 'q':
-                self.capture
+    def capture(self, frame):
 
-    def capture(self):
-        with Listener(on_press=self.on_press) as listener:
-            listener.join()
-            # define a video capture object
-            vid = cv2.VideoCapture(self.rtsp_path)
-
-            # Capture the video frame
-            # by frame
-            print('------------ starting video ---------------')
-            while(True):
-                success, frame = vid.read()
-            if cv2.waitKey(1) & 0xFF == ord('c'):
-                # Display the resulting frame
-                if success:
-                    cv2.imwrite(
-                        self.out_path,
-                        frame)
-                    cv2.imshow("TEST", frame)
-                    print(type(frame))
-                    return frame
+        # Capture the video frame
+        # by frame
+        print('------------ starting video ---------------')
+        try:
+            cv2.imwrite(
+                f'{self.out_path}',
+                frame)
+            print('Video captured')
+            
+        except Exception as e:
+            print(str(e))
+            pass
